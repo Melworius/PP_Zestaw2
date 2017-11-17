efbc416deb6aba4ceb08a0c3c68ce8ef00f99064
PAWEŁ <PAWE@192.168.1.71>
2017-11-17 18:55:39 +0100

    a

--- src/Zad4.java
+++ src/Zad4.java
@@ -5,26 +5,19 @@
 import java.util.Scanner;
 public class Zad4 {
 
-	public static void main(String[] args) {
-		System.out.println("Podaj liczbê: ");
-		int p;
-		int suma;
-		Scanner liczba = new Scanner(System.in);
-		p = liczba.nextInt();
-		do {
-			//System.out.println("Podaj liczbê: ");
-			 
-			if (p != 0) {
-				System.out.println("Podaj liczbê: ");
-				p = liczba.nextInt();
-			} else {
-				suma = p;
-				System.out.println(suma);
-			}	
-			}
-			while (p != 0);
-			
-			liczba.close();
+    public static void main(String[] args) {
+        int p;
+        int suma = 0;
+        Scanner liczba = new Scanner(System.in);
+
+        do {
+            System.out.print("Podaj liczbe: ");
+            p = liczba.nextInt();
+            suma += p;
+        } while (p != 0);
+
+        System.out.println("Suma to: " + suma);
+        liczba.close();
 		}
 	}
 
